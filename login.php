d286abfcf6c4a32c3b2cdbf0acfca3b1321e82a7
NicolaDB <78536909+NicolaDB@users.noreply.github.com>
2021-02-06 10:53:35 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="navbar.css">
+    <title>PHP index page</title>
+  </head>
+  <body>
+      <nav><!--it should reamin always the same, we have attached a css file only for styling the navbar-->
+        <a class="nav_elements" href="index.php"></a><img src="" alt="">
+        <ul>
+          <li><a class="nav_elements" href="discover.php"></a>About us</li>
+          <li><a class="nav_elements" href="blog.php"></a>Find Blogs</li>
+          <li><a class="nav_elements" href="signup.php"></a>Sign up</li>
+          <li><a class="nav_elements" href="login.php"></a>Login</li>
+        </ul>
+      </nav>
+
+    </div><!--end of the wrapper-->
+  </body>
+</html>
