e75fb68b20fec4f7c01e0e666d99380ffcdfea37
NicolaDB <78536909+NicolaDB@users.noreply.github.com>
2021-02-06 12:02:02 +0100

    1.2

--- login.php
+++ login.php
@@ -11,10 +11,10 @@
       <nav><!--it should reamin always the same, we have attached a css file only for styling the navbar-->
         <a class="nav_elements" href="index.php"></a><img src="" alt="">
         <ul>
-          <li><a class="nav_elements" href="discover.php"></a>About us</li>
-          <li><a class="nav_elements" href="blog.php"></a>Find Blogs</li>
-          <li><a class="nav_elements" href="signup.php"></a>Sign up</li>
-          <li><a class="nav_elements" href="login.php"></a>Login</li>
+          <li><a class="nav_elements" href="discover.html">About us</a></li>
+          <li><a class="nav_elements" href="blog.html">Find Blogs</a></li>
+          <li><a class="nav_elements" href="signup.html">Sign up</a></li>
+          <li><a class="nav_elements" href="login.html">Login</a></li>
         </ul>
       </nav>
 
